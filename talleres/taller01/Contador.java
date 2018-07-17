486c483c2109195ebf7596ce56b5032c23c74217
ManuelG28 <36460775+ManuelG28@users.noreply.github.com>
2018-07-17 16:34:33 -0500

    Update Contador.java
    version final.

--- talleres/taller01/Contador.java
+++ talleres/taller01/Contador.java
@@ -1,17 +1,48 @@
 public class Contador {
-    public Contador() {
+    
+	private int cuenta;
+	private final String id;
 
+
+	/**
+     * Se inicializan las variables globales en el constructor de manera que no posean valores nulos o 0s.
+     */
+    public Contador(String id) {
+        this.id = id;
     }
 
+    /**
+     * El método incrementar incrementa el contador en una unidad.
+     *
+     *(opcional: se podria pasar un parametro "cantidad" para incrementar esa cantidad de unidades).
+     */
     public void incrementar() {
-
+        cuenta++;
     }
 
+    /*
+    Se podría implementar un metodo decrementar para hacer la operacion inversa de incrementar.
+    */
+
+    /**
+     * El método incrementos nos muestra en que valor se encuentra la cuenta actualmente.
+     * @return el numero actual de la cuenta
+     */
     public int incrementos() {
-
+        return cuenta;
     }
 
+    
+     /**
+    * toString se encargará de convertir el contador en un tipo cadena
+    * para su posterior visualización
+    * se debe de mostrar el estado del contador y su id
+    *
+    * @return una cadena que contiene el id del contador y su cuenta
+    */
     public String toString() {
-
+        String contador = "";
+        contador = cuenta+" id:"+id;
+        return contador;
     }
 }
