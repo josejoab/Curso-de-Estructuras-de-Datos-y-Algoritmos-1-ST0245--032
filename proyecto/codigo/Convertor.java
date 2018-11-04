fa0cadf4937256f196874e028c96ad6bcfb95d0d
ManuelG28 <36460775+ManuelG28@users.noreply.github.com>
2018-11-04 18:49:22 -0500

    Create Convertor.java

---
+++ proyecto/codigo/Convertor.java
@@ -0,0 +1,24 @@
+
+package Proyecto2;
+
+/**
+ *
+ * @author Manuel Gutierrez
+ * @author Kevin Herrera
+ * @author Joab Romero
+ */
+public class Convertor {
+    
+    public int gradToX(double x){
+        return (int)(x*111111);
+    }
+    
+    public int gradToY(double y){
+        return (int)(y*111111);
+    }
+    
+    public int gradToZ(double z){
+        return (int)(z);
+    }
+    
+}
