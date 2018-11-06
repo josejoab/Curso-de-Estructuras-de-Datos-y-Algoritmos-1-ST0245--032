73531371aa3c01efb509f9cbe9e787fb028a1fde
ManuelG28 <36460775+ManuelG28@users.noreply.github.com>
2018-11-05 21:17:39 -0500

    Agregue documentación al código

--- proyecto/codigo/Convertor.java
+++ proyecto/codigo/Convertor.java
@@ -1,22 +1,32 @@
-
-package Proyecto2;
-
 /**
- *
+ * En este clase convertimos los grados a coordenadas cartesianas.
  * @author Manuel Gutierrez
  * @author Kevin Herrera
  * @author Joab Romero
  */
+
 public class Convertor {
-    
+    /**
+     * Recibe la latitud y la convierte a x.
+     * @param x
+     * @return 
+     */
     public int gradToX(double x){
         return (int)(x*111111);
     }
-    
+     /**
+     * Recibe la longitud y la convierte a y.
+     * @param y
+     * @return 
+     */
     public int gradToY(double y){
         return (int)(y*111111);
     }
-    
+    /**
+     * Recibe la altitud y la convierte a z
+     * @param z
+     * @return 
+     */
     public int gradToZ(double z){
         return (int)(z);
     }
