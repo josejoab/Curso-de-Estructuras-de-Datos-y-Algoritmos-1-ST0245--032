e40ee9f4df435a48fb7390800c4d4a721a2d5b45
ManuelG28 <36460775+ManuelG28@users.noreply.github.com>
2018-11-05 21:19:19 -0500

    Agregue documentación al codigo

--- proyecto/codigo/MapaBello.java
+++ proyecto/codigo/MapaBello.java
@@ -1,12 +1,10 @@
-
-package Proyecto2;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
+ * Representación del mapa de bello como una clase.
  * @author Manuel Gutierrez
  * @author Kevin Herrera
  * @author Joab Romero
@@ -14,7 +12,9 @@ import java.util.HashMap;
 
 public class MapaBello{
     
-    //atributos
+    /**
+     * Atributos de la clase MapaBello.
+     */
     
     int alto;
     int ancho;
@@ -24,12 +24,11 @@ public class MapaBello{
     int profunBoxes;
     int numeroCuadrosP;
     
-    HashMap<Integer,ArrayList<Abeja>> mapa = new HashMap<Integer,ArrayList<Abeja>>();  
-    ArrayList<Abeja> arregloDeAbejas;
+    HashMap<Integer,ArrayList<Abeja>> mapa = new HashMap<Integer,ArrayList<Abeja>>();  //HashMap donde guardamos las abejas
+    ArrayList<Abeja> arregloDeAbejas; // Arreglo de abejas esparcidas por el mapa.
     
-    ArrayList<Abeja> abejasColisionadas = new ArrayList<>();
-    
-    //metodos
+    ArrayList<Abeja> abejasColisionadas = new ArrayList<>(); //Abejas con riesgo de colisión
+
     
     public MapaBello(ArrayList<Abeja> arregloDeAbejas) throws InterruptedException {
         Lector lector = new Lector();
@@ -53,7 +52,9 @@ public class MapaBello{
     public int getProfundidad(){
         return profundidad;
     }
-    
+    /**
+     * Creamos el hash map de int a ArrayList de abejas.
+     */
     public void obtenerAbejas(){
         for (int i = 0; i < arregloDeAbejas.size(); i++){
             int id = ubicarAbeja(arregloDeAbejas.get(i).getX(), arregloDeAbejas.get(i).getY(),
@@ -64,7 +65,9 @@ public class MapaBello{
             mapa.get(id).add(arregloDeAbejas.get(i));
         }
     }
-    
+    /**
+     * Las agregamos y las agrupamos en un conjunto del hashmap.
+     */
     public void agrupar(){
         for(HashMap.Entry<Integer, ArrayList<Abeja>> i: mapa.entrySet()){
              if(i.getValue().size() > 1){
@@ -74,7 +77,9 @@ public class MapaBello{
              }
         }
     }
-    
+    /**
+     * Verificamos si una abeja esta sola para encontrar en los cuadros adyacentes su respectiva colisión
+     */
     public void encontrarSolas(){
         
         int cf = (int)((ancho/anchoBoxes)*(alto/altoBoxes));
@@ -159,14 +164,23 @@ public class MapaBello{
         
         return false;
     }
-    
+    /**
+     * Formula que nos ubica la abeja en el hash map dandole un ID.
+     * @param x
+     * @param y
+     * @param z
+     * @return 
+     */
     public int ubicarAbeja(int x, int y,int z){
         return ((int)(Math.floor(x/anchoBoxes)) +
                 (int)(Math.floor(y/altoBoxes) * (ancho/anchoBoxes))+
                 (int)(Math.floor(z/profunBoxes) *
                      ((ancho/anchoBoxes) * (alto/altoBoxes))));
     }
-    
+    /**
+     * Divide el mapa en cuadros pequeños para X
+     * @param x 
+     */
     public void dividirX(int x){
         boolean salir = false;
         while(anchoBoxes > x && salir == false){  
@@ -188,7 +202,10 @@ public class MapaBello{
         }
         this.numeroCuadrosP = (((ancho/anchoBoxes)*(alto/altoBoxes))*(profundidad/profunBoxes));
     }
-    
+    /**
+     * Divide el mapa en cuadros pequeños para Y
+     * @param y 
+     */
     public void dividirY(int y){
         boolean salir = false;
         while(altoBoxes > y && salir == false){
@@ -210,7 +227,10 @@ public class MapaBello{
         }
         this.numeroCuadrosP=(((ancho/anchoBoxes)*(alto/altoBoxes))*(profundidad/profunBoxes));
     }
-    
+    /**
+     * Divide el mapa con cuadros pequeños para Z
+     * @param z 
+     */
     public void dividirZ(int z){
         boolean salir = false;
         while(profunBoxes > z && salir == false){
@@ -233,18 +253,19 @@ public class MapaBello{
         }
         this.numeroCuadrosP=(((ancho/anchoBoxes)*(alto/altoBoxes))*(profundidad/profunBoxes));
     }
+    /**
+     * Guarda las abejas colisionadas.
+     * @param abejasConRiesgoDeColision
+     * @param numeroDeAbejas 
+     */
     
     public void guardarColisiones(ArrayList<Abeja> abejasConRiesgoDeColision, int numeroDeAbejas){
-          final String nombreDelArchivo = "respuestaConjuntoDeDatosCon"+numeroDeAbejas+"abejas.txt";  
-          int cont = 0;
-          
+          final String nombreDelArchivo = "respuestaConjuntoDeDatosCon"+numeroDeAbejas+"abejas.txt";   
           try {
              PrintWriter escritor = new PrintWriter(nombreDelArchivo, "UTF-8");
              for (Abeja abeja : abejasConRiesgoDeColision){
                 escritor.println(abeja.getX()+","+abeja.getY()+","+abeja.getZ());
-                 cont++;
              }
-              System.out.println("Nùmero de abejas: " + cont);
              escritor.close();
           }
           catch(IOException ioe) {
@@ -252,4 +273,6 @@ public class MapaBello{
           }  
     }
     
+}
+
 }
