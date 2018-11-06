eae6a2c44c8f5350115113802a045c60df858ccc
ManuelG28 <36460775+ManuelG28@users.noreply.github.com>
2018-11-05 21:16:59 -0500

    Agregue documentación al código

--- proyecto/codigo/Abeja.java
+++ proyecto/codigo/Abeja.java
@@ -1,5 +1,3 @@
-package Proyecto2;
-
 import java.util.ArrayList;
 
 /**
@@ -12,7 +10,10 @@ import java.util.ArrayList;
 
 public class Abeja {
     
-    //Atributos
+    /**
+     * La clase abeja tiene cuatro atributos, sus coordenadas en x,y,z y
+     * su rango de colisión predeterminado.
+     */
     
     private int x;
     private int y;
@@ -20,7 +21,13 @@ public class Abeja {
     private int rangoColision;
     ArrayList<Abeja> abejas = new ArrayList();
     
-    //metodos
+    /**
+     * 
+     * @param x latitud convertida a coordenadas cartesianas 
+     * @param y longitud convertidad a coordenadas cartesianas
+     * @param z altitud convertidad a coordenadas cartesianas
+     * @param RG rango de colisión
+     */
     
     public Abeja(int x, int y, int z, int RG) {
         this.x = x;
@@ -28,18 +35,31 @@ public class Abeja {
         this.z = z;
         this.rangoColision = RG;
     }
+    /**
+     * Metodo getter para x.
+     * @return x
+     */
     public int getX() {
         return x;
     }
-
+    /**
+     * Metodo getter para y
+     * @return y
+     */
     public int getY() {
         return y;
     }
-
+    /**
+     * Metodo getter para z
+     * @return z
+     */
     public int getZ() {
         return z;
     }
-
+    /**
+     * Metodo getter para rangoColision
+     * @return rangoColision
+     */
     public int getRangoColision() {
         return rangoColision;
     }
